884165bf08075e8beffa1f4e5d9f7dc124ed7a6c
willian portela <willianportela1@gmail.com>
2022-07-28 19:51:22 -0300

    requisito 5 6 e 7

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Routes from './routes/Routes';
 
 function App() {
-  return <Routes/>;
+  return <Routes />;
 }
 
 export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,77 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Table extends Component {
+class Table extends Component {
+  editExpense = () => {};
+
+  deleteExpense = () => {};
+
   render() {
+    const headerList = [
+      'Descrição',
+      'Tag',
+      'Método de pagamento',
+      'Valor',
+      'Moeda',
+      'Câmbio utilizado',
+      'Valor convertido',
+      'Moeda de conversão',
+      'Editar/Excluir',
+    ];
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            {headerList.map((column) => (
+              <th key={ column }>{column}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{Number(expense.value).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>
+                {Math.round(
+                  Number(expense.exchangeRates[expense.currency].ask) * 100,
+                ) / 100}
+              </td>
+              <td>
+                {Math.round(
+                  Number(expense.value)
+                    * Number(expense.exchangeRates[expense.currency].ask)
+                    * 100,
+                ) / 100}
+              </td>
+              <td>BRL</td>
+              <td>
+                <button type="button" onClick={ this.editExpense() }>
+                  Editar
+                </button>
+                <button type="button" onClick={ this.deleteExpense() }>
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
